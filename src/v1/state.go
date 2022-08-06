7ba6c95263e2a12914fe7b4af09a0d45a4d7127e
lewis metcalf <leweymetcalf@hotmail.co.uk>
2022-08-06 16:07:24 +0200

    v1

---
+++ src/v1/state.go
@@ -0,0 +1,43 @@
+package v1
+
+type Status string
+
+const (
+	Success Status = "success"
+	Fail           = "fail"
+	Normal         = "normal"
+)
+
+type Predicate func(input rune) bool
+
+type destination *State
+
+type Transition struct {
+	// to: a pointer to the next state
+	to destination
+	// predicate: a function to determine if the runner should move to the next state
+	predicate Predicate
+}
+
+type State struct {
+	id          int
+	transitions []Transition
+}
+
+func (s *State) firstMatchingTransition(input rune) destination {
+	for _, t := range s.transitions {
+		if t.predicate(input) {
+			return t.to
+		}
+	}
+
+	return nil
+}
+
+func (s *State) isSuccessState() bool {
+	if len(s.transitions) == 0 {
+		return true
+	}
+
+	return false
+}
