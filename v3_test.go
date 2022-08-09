67aa8cb951c047651920c8f27e30f5dc789c5d38
lewis metcalf <leweymetcalf@hotmail.co.uk>
2022-08-09 11:14:14 +0200

    v3

---
+++ v3_test.go
@@ -0,0 +1,41 @@
+package v3
+
+import (
+	"testing"
+)
+
+func TestCompiledFSM(t *testing.T) {
+	parser := NewParser()
+
+	tokens := lex("abc")
+	ast := parser.Parse(tokens)
+	startState, _ := ast.compile()
+
+	type test struct {
+		name           string
+		input          string
+		expectedStatus Status
+	}
+
+	tests := []test{
+		{"empty string", "", Normal},
+		{"non matching string", "xxx", Fail},
+		{"matching string", "abc", Success},
+		{"partial matching string", "ab", Normal},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			testRunner := NewRunner(startState)
+
+			for _, character := range tt.input {
+				testRunner.Next(character)
+			}
+
+			result := testRunner.GetStatus()
+			if tt.expectedStatus != result {
+				t.Fatalf("Expected FSM to have final state of '%v', got '%v'", tt.expectedStatus, result)
+			}
+		})
+	}
+}
