5505c7710957ee4cf7b940cf91865610d4581b7f
lewis metcalf <leweymetcalf@hotmail.co.uk>
2022-08-18 21:26:07 +0200

    v3 - adding regex package

---
+++ src/v3/regex.go
@@ -0,0 +1,37 @@
+package v3
+
+type myRegex struct {
+	fsm *State
+}
+
+func NewMyRegex(re string) *myRegex {
+	tokens := lex(re)
+	parser := NewParser()
+	ast := parser.Parse(tokens)
+	state, _ := ast.compile()
+	return &myRegex{fsm: state}
+}
+
+func (m *myRegex) MatchString(input string) bool {
+	testRunner := NewRunner(m.fsm)
+	return match(testRunner, []rune(input))
+}
+
+func match(runner *runner, input []rune) bool {
+	runner.Reset()
+
+	for _, character := range input {
+		runner.Next(character)
+		status := runner.GetStatus()
+
+		if status == Fail {
+			return match(runner, input[1:])
+		}
+
+		if status == Success {
+			return true
+		}
+	}
+
+	return runner.GetStatus() == Success
+}
