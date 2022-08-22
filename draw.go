76d8b936c030bfc3ec800559a511330a1c40673f
lewis metcalf <leweymetcalf@hotmail.co.uk>
2022-08-22 11:32:10 +0200

    v5 - refactor draw and add comments

--- draw.go
+++ draw.go
@@ -5,27 +5,6 @@ import (
 	"strings"
 )
 
-type StateMap struct {
-	nextId   int
-	stateMap map[*State]int
-}
-
-func (sm *StateMap) getId(state *State) int {
-	if sm.stateMap == nil {
-		sm.stateMap = make(map[*State]int)
-	}
-
-	id, inMap := sm.stateMap[state]
-	if !inMap {
-		i := sm.nextId
-		sm.stateMap[state] = sm.nextId
-		sm.nextId++
-		return i
-	}
-
-	return id
-}
-
 func (s *State) Draw() string {
 	res := []string{
 		"graph LR",
@@ -43,10 +22,39 @@ func draw(sm StateMap, s *State) []string {
 	currentId := sm.getId(s)
 	for _, transition := range s.transitions {
 		destinationId := sm.getId(transition.to)
+
+		// add the transition to the list
 		res = append(res, fmt.Sprintf("%d((%d)) -- %s --> %d((%d))", currentId, currentId, transition.debugSymbol, destinationId, destinationId))
 
+		// recursively add all the transitions from the child nodes and flatten the list
 		res = append(res, draw(sm, transition.to)...)
 	}
 
 	return res
+}
+
+// StateMap is used to cache ids for States
+type StateMap struct {
+	nextId   int
+	stateMap map[*State]int
+}
+
+// getId will return an incrementing numerical id for each state
+func (sm *StateMap) getId(state *State) int {
+	// initialize map
+	if sm.stateMap == nil {
+		sm.stateMap = make(map[*State]int)
+	}
+
+	id, inMap := sm.stateMap[state]
+	if !inMap {
+		// if state is not in the cache, store the state and increment the id
+		i := sm.nextId
+		sm.stateMap[state] = sm.nextId
+		sm.nextId++
+		return i
+	}
+
+	// else, just return the cached id
+	return id
 }
