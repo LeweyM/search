b9a6480a32fc36c89d397105a5cd77fe01382816
lewis metcalf <leweymetcalf@hotmail.co.uk>
2022-08-22 10:43:56 +0200

    v5 - init

---
+++ src/v5/ast.go
@@ -0,0 +1,55 @@
+package v5
+
+type Node interface {
+	compile() (head *State, tail *State)
+}
+
+type CompositeNode interface {
+	Node
+	Append(node Node)
+}
+
+type Group struct {
+	ChildNodes []Node
+}
+
+type CharacterLiteral struct {
+	Character rune
+}
+
+func (g *Group) Append(node Node) {
+	g.ChildNodes = append(g.ChildNodes, node)
+}
+
+type WildcardLiteral struct{}
+
+/* Compiler methods */
+
+func (g *Group) compile() (head *State, tail *State) {
+	startState := State{}
+	currentTail := &startState
+
+	for _, expression := range g.ChildNodes {
+		nextStateHead, nextStateTail := expression.compile()
+		currentTail.merge(nextStateHead)
+		currentTail = nextStateTail
+	}
+
+	return &startState, currentTail
+}
+
+func (l CharacterLiteral) compile() (head *State, tail *State) {
+	startingState := State{}
+	endState := State{}
+
+	startingState.addTransition(&endState, func(input rune) bool { return input == l.Character })
+	return &startingState, &endState
+}
+
+func (w WildcardLiteral) compile() (head *State, tail *State) {
+	startingState := State{}
+	endState := State{}
+
+	startingState.addTransition(&endState, func(input rune) bool { return input != '\n' })
+	return &startingState, &endState
+}
