e415d0f020280142a87177287c6e97de0c66494a
lewis metcalf <leweymetcalf@hotmail.co.uk>
2022-08-29 09:25:03 +0200

    v5 - draw the runner with current state

--- main.go
+++ main.go
@@ -12,38 +12,80 @@ import (
 func Main(args []string) {
 	switch args[0] {
 	case "draw":
-		Draw(args[1])
+		if len(args) == 2 {
+			RenderFSM(args[1])
+		} else if len(args) == 3 {
+			RenderRunner(args[1], args[2])
+		}
 	default:
 		fmt.Println("command not recognized")
 	}
 }
 
-func Draw(input string) {
+func RenderFSM(input string) {
 	tokens := lex(input)
 	parser := NewParser()
 	ast := parser.Parse(tokens)
 	head, _ := ast.compile()
 
-	stateDrawing := head.Draw()
+	graph, _ := head.Draw()
 
-	t, err := template.New("graph").Parse(`
-<script src="https://cdn.jsdelivr.net/npm/mermaid/dist/mermaid.min.js"></script>
-<script>mermaid.initialize({startOnLoad:true});
-</script>
-<div class="mermaid">
-    {{ . }}
-</div>
-<div>
-<span style="white-space: pre-wrap">{{ . }}</span>
-</div>
-`)
+	renderTemplateToBrowser(fsmTemplate, graph)
+}
+
+// RenderRunner will render every step of the runner until it fails or succeeds. The template will then take care
+// of hiding all but one of the steps to give the illusion of stepping through the input characters.
+func RenderRunner(regex, input string) {
+	tokens := lex(regex)
+	parser := NewParser()
+	ast := parser.Parse(tokens)
+	head, _ := ast.compile()
+
+	allGraphSteps := NewRunner(head).drawAllGraphSteps(input)
+
+	var graphs []graphData
+	for letterIndex, graphStep := range allGraphSteps {
+		graphs = append(graphs, graphData{
+			Graph: graphStep,
+			Input: splitSearchString(input, letterIndex),
+		})
+	}
+
+	renderTemplateToBrowser(runnerTemplate, templateData{
+		Graphs: graphs,
+		Regex:  regex,
+	})
+}
+
+// splitSearchString divides the search input string into three pieces so that we can render in the browser:
+// 1. What has already been processed
+// 2. The next character to process
+// 3. What is yet to be processed
+func splitSearchString(input string, currentLetterIndex int) inputSearchString {
+	var left, middle, right string
+
+	left = input[:currentLetterIndex]
+	if currentLetterIndex < len(input) {
+		middle = string(input[currentLetterIndex])
+		right = input[currentLetterIndex+1:]
+	}
+
+	return inputSearchString{
+		Left:   left,
+		Middle: middle,
+		Right:  right,
+	}
+}
+
+func renderTemplateToBrowser(tmplt string, data any) {
+	t, err := template.New("graph").Parse(tmplt)
 	if err != nil {
 		panic(err)
 	}
 	w := bytes.Buffer{}
-	err = t.Execute(&w, stateDrawing)
+	err = t.Execute(&w, data)
 	if err != nil {
-		return
+		panic(err)
 	}
 
 	reader := strings.NewReader(w.String())
