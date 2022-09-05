4bc6e17bbb02f447bcc5b751fd20cd6db3f5f618
lewis metcalf <leweymetcalf@hotmail.co.uk>
2022-09-05 12:10:42 +0200

    v8 - init

---
+++ transition.go
@@ -0,0 +1,32 @@
+package v8
+
+import (
+	"strings"
+)
+
+type Transition struct {
+	debugSymbol string
+	// to: a pointer to the next state
+	to        *State
+	from      *State
+	predicate Predicate
+}
+
+type Predicate struct {
+	allowedChars    string
+	disallowedChars string
+}
+
+func (p Predicate) test(input rune) bool {
+	if p.allowedChars != "" && p.disallowedChars != "" {
+		panic("must be mutually exclusive")
+	}
+
+	if len(p.allowedChars) > 0 {
+		return strings.ContainsRune(p.allowedChars, input)
+	}
+	if len(p.disallowedChars) > 0 {
+		return !strings.ContainsRune(p.disallowedChars, input)
+	}
+	return false
+}
