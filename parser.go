f24bd7b4a320dfc4f7ba2039bf14eeb7de30fbf5
lewis metcalf <leweymetcalf@hotmail.co.uk>
2022-09-20 12:05:07 +0200

    retroactive - simplify parser until groups

--- parser.go
+++ parser.go
@@ -1,44 +1,24 @@
 package v4
 
 type Parser struct {
-	fsmStack []CompositeNode
-	tokens   []token
+	tokens []token
 }
 
 func NewParser(tokens []token) *Parser {
-	return &Parser{fsmStack: []CompositeNode{}, tokens: tokens}
+	return &Parser{tokens: tokens}
 }
 
 func (p *Parser) Parse() Node {
-	p.pushNewGroup()
+	root := Group{}
 
 	for _, t := range p.tokens {
 		switch t.symbol {
 		case Character:
-			node := p.pop()
-			node.Append(CharacterLiteral{Character: t.letter})
-			p.push(node)
+			root.Append(CharacterLiteral{Character: t.letter})
 		case AnyCharacter:
-			node := p.pop()
-			node.Append(WildcardLiteral{})
-			p.push(node)
+			root.Append(WildcardLiteral{})
 		}
 	}
 
-	return p.pop()
-}
-
-func (p *Parser) pushNewGroup() {
-	p.push(&Group{})
-}
-
-func (p *Parser) pop() CompositeNode {
-	pop := p.fsmStack[len(p.fsmStack)-1]
-	p.fsmStack = p.fsmStack[:len(p.fsmStack)-1]
-
-	return pop
-}
-
-func (p *Parser) push(g CompositeNode) {
-	p.fsmStack = append(p.fsmStack, g)
+	return &root
 }
