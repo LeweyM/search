3d21440ebb2fb9fa118f3d3f7e29242e07739325
lewis metcalf <leweymetcalf@hotmail.co.uk>
2022-10-30 21:30:58 +0100

    retroactive - remove incoming transitions on states

--- state.go
+++ state.go
@@ -1,7 +1,5 @@
 package v7
 
-import "fmt"
-
 type Status string
 
 const (
@@ -13,7 +11,6 @@ const (
 type State struct {
 	transitions []Transition
 	epsilons    []*State
-	incoming    []*State
 }
 
 func (s *State) matchingTransitions(input rune) []*State {
@@ -43,22 +40,14 @@ func (s *State) addTransition(destination *State, predicate Predicate, debugSymb
 		predicate:   predicate,
 	}
 	s.transitions = append(s.transitions, t)
-	destination.incoming = append(destination.incoming, s)
 }
 
 func (s *State) addEpsilon(destination *State) {
 	s.epsilons = append(s.epsilons, destination)
-	destination.incoming = append(destination.incoming, s)
 }
 
 // adds the transitions of other State (s2) to this State (s).
-//
-// warning: do not use if State s2 has any incoming transitions.
 func (s *State) merge(s2 *State) {
-	if len(s2.incoming) != 0 {
-		panic(fmt.Sprintf("State (%+v) cannot be merged if it has any incoming transitions. It has incoming transitions from the following states; %+v", *s2, s.incoming))
-	}
-
 	for _, t := range s2.transitions {
 		// 1. copy s2 transitions to s
 		s.addTransition(t.to, t.predicate, t.debugSymbol)
@@ -69,22 +58,6 @@ func (s *State) merge(s2 *State) {
 }
 
 func (s *State) delete() {
-	// 1. remove s from incoming of connected nodes.
-	for _, t := range s.transitions {
-		t.to.removeIncoming(s)
-	}
-
-	// 2. remove the outgoing transitions
 	s.transitions = nil
 	s.epsilons = nil
-}
-
-func (s *State) removeIncoming(target *State) {
-	var newIncoming []*State
-	for _, state := range s.incoming {
-		if target != state {
-			newIncoming = append(newIncoming, state)
-		}
-	}
-	s.incoming = newIncoming
 }
