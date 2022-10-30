2dab2af905609c28509fcbd8d2c0e9b0efb5dd77
lewis metcalf <leweymetcalf@hotmail.co.uk>
2022-10-30 18:26:34 +0100

    retroactive - update orderedset methods

--- src/v8/orderedset.go
+++ src/v8/orderedset.go
@@ -1,19 +1,25 @@
 package v8
 
+import (
+	"sort"
+)
+
 // OrderedSet maintains an ordered set of unique items of type <T>
 type OrderedSet[T comparable] struct {
 	set       map[T]int
 	nextIndex int
 }
 
-func (o *OrderedSet[T]) add(t T) {
+func (o *OrderedSet[T]) add(ts ...T) {
 	if o.set == nil {
 		o.set = make(map[T]int)
 	}
 
-	if !o.has(t) {
-		o.set[t] = o.nextIndex
-		o.nextIndex++
+	for _, t := range ts {
+		if !o.has(t) {
+			o.set[t] = o.nextIndex
+			o.nextIndex++
+		}
 	}
 }
 
@@ -26,9 +32,15 @@ func (o *OrderedSet[T]) list() []T {
 	size := len(o.set)
 	list := make([]T, size)
 
-	for t, i := range o.set {
+	i := 0
+	for t := range o.set {
 		list[i] = t
+		i++
 	}
+
+	sort.Slice(list, func(i, j int) bool {
+		return o.getIndex(list[i]) < o.getIndex(list[j])
+	})
 
 	return list
 }
