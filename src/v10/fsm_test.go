81794141419a76aead920c5d929399fbb1a818ae
lewis metcalf <leweymetcalf@hotmail.co.uk>
2022-11-03 19:19:57 +0100

    retroactive - refactor fsm test code

--- src/v10/fsm_test.go
+++ src/v10/fsm_test.go
@@ -7,54 +7,6 @@ import (
 	"testing"
 )
 
-func TestCompiledFSM(t *testing.T) {
-	tokens := lex("abc")
-	parser := NewParser(tokens)
-	ast := parser.Parse()
-	startState, _ := ast.compile()
-
-	type test struct {
-		name           string
-		input          string
-		expectedStatus Status
-	}
-
-	tests := []test{
-		{"empty string", "", Normal},
-		{"non matching string", "xxx", Fail},
-		{"matching string", "abc", Success},
-		{"partial matching string", "ab", Normal},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			testRunner := NewRunner(startState)
-
-			for _, character := range tt.input {
-				testRunner.Next(character)
-			}
-
-			result := testRunner.GetStatus()
-			if tt.expectedStatus != result {
-				t.Fatalf("Expected FSM to have final state of '%v', got '%v'", tt.expectedStatus, result)
-			}
-
-			goRegex := regexp.MustCompile("abc")
-			match := goRegex.Match([]byte(tt.input))
-
-			if tt.expectedStatus == Success {
-				if !match {
-					t.Fatalf("Expected FSM to have same result as GORegex pkg. Expected '%v', got '%v'", tt.expectedStatus, match)
-				}
-			} else {
-				if match {
-					t.Fatalf("Expected FSM to have same result as GORegex pkg. Expected '%v', got '%v'", tt.expectedStatus, match)
-				}
-			}
-		})
-	}
-}
-
 func TestFSMAgainstGoRegexPkg(t *testing.T) {
 	type test struct {
 		name  string
